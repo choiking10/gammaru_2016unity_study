ed870e752f511efbc6ff3d752c6e8d002f6ba8cb
최윤호 <choiking10@naver.com>
2016-04-10 17:06:51 +0900

    start

---
+++ unity_study/Assets/MoveInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveInput : MonoBehaviour {
+    public Rigidbody mybody;
+    public float forcepower;
+	// Use this for initialization
+	void Start () {
+        mybody = gameObject.GetComponent<Rigidbody>();
+        forcepower = 10;
+	}
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            mybody.AddForce(transform.right * forcepower);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            mybody.AddForce(transform.forward * forcepower);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            mybody.AddForce(-transform.right * forcepower);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            mybody.AddForce(-transform.forward * forcepower);
+        }
+	}
+}
