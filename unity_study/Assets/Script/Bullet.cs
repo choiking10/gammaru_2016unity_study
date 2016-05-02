77459079c393bcc90d45d5f16f6f2cf43a061fb8
최윤호 <choiking10@naver.com>
2016-05-02 18:34:44 +0900

    add explosion

--- unity_study/Assets/Script/Bullet.cs
+++ unity_study/Assets/Script/Bullet.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
     public GameObject shooter;
-
+    public GameObject explosion;
 	// Use this for initialization
 	void Start () {
 	    
@@ -13,11 +13,21 @@ public class Bullet : MonoBehaviour {
 	void Update () {
 	    
 	}
+    void Destroy_bullet()
+    {
+        Destroy(gameObject);
+    }
     void OnTriggerEnter(Collider col)
     {
         if (col.tag.CompareTo("map") == 0)
         {
-            Destroy(gameObject);
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().useGravity = false;
+            GameObject obj = (GameObject)GameObject.Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
+            obj.transform.parent = gameObject.transform;
+
+            Invoke("Destroy_bullet", 0.5f);
         }
     }
 }
